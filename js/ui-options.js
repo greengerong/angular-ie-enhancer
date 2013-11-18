d9182fa113e8ded1f72ab1e30fd42ebe510d1765
greengerongg <grzx2210@163.com>
2013-11-18 22:26:12 +0800

    init -spike

---
+++ js/ui-options.js
@@ -0,0 +1,21 @@
+angular.module('ui-options', [])
+    .directive('uiOptions', [function () {
+
+        return  function (scope, elm, iAttrs) {
+            scope.$watch(iAttrs.uiOptions, function (val) {
+                console.log(val, "$watch");
+                renderOption(val);
+            }, true);
+
+            var renderOption = function (val) {
+                elm.find("option").remove();
+                if (val) {
+                    var options = [];
+                    angular.forEach(val, function (obj) {
+                        options.push("<option value='" + obj.value + "' >" + obj.text + "</option>")
+                    });
+                    elm.append(options.join(""));
+                }
+            };
+        };
+    }]);
